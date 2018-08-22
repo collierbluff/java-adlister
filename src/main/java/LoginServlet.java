341847118b0cd8434de1ad05e568c33e425e35aa
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-08-22 16:57:02 -0500

    servlet work

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,45 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("validUser") == null){
+
+            session.invalidate();
+
+        }else if((boolean)session.getAttribute("validUser")){
+
+            response.sendRedirect("/profile");
+            return;
+        }
+
+
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
+
         if (validAttempt) {
+
+            HttpSession session = request.getSession();
+
+            session.setAttribute("user", username);
+            session.setAttribute("validUser", true);
+
             response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
         }
     }
 }
